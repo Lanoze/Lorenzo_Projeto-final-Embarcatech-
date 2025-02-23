32942eb97628212e922569b5e8af0b2f359c2c52
Lanoze <lorenzo.oliveira@discente.univasf.edu.br>
2025-02-23 13:21:10 -0300

    O cronometro esta funcionando, mas a precisão não parece ser das melhores, mesmo usando repeating time callback

--- DisplayC.c
+++ DisplayC.c
@@ -25,17 +25,22 @@
 #define VRY_PIN 27   
 #define JOYSTICK_BUTTON 22
 
-uint32_t last_time=0,last_time2=0;
+uint32_t last_time=0,last_time2=0,last_time3=0,time_teste=0,start_time;
 ssd1306_t ssd; // Inicializa a estrutura do display
-bool leds_ativos=1,cor=1,pressed=0;
+bool leds_ativos=1,cor=1,pressed=0,start=0;
 uint8_t teste1,teste2;
 uint8_t rec_pos=0; //Vai de 0 a 2
 uint8_t inicio_display=0; //A partir de qual string vai desenhar em opt_list, só pode ser 0 ou 1 com as 4 opções
 uint8_t selected=0; //0=Cronometro, 1=Temporizador, 2=Ajustar hora, 3=Alarme, 4 ou mais=Inválido
+uint8_t valor_exibido[6];//Valores exibidos no cronômetro
+uint32_t debug_aux=0;//Variável pra ajudar na depuração
+struct repeating_timer timer1;
+char tempo[8];
 
 void draw_options();
 void cronometro();
 void temporizador();
+void incrementar_cronometro();
 
 //Rotina de interrupção
 void interrupt(uint gpio, uint32_t events)
@@ -58,6 +63,44 @@ void interrupt(uint gpio, uint32_t events)
     }
 }
 
+bool timer_cronometro(struct repeating_timer *timer){
+  //uint64_t elapsed = (time_us_64() - start_time) / 1000;  // Convertendo microssegundos para milissegundos
+   // printf("Tempo: %llu ms\n", elapsed);
+ if(start)
+ {incrementar_cronometro(); return true;}
+ return false;
+}
+
+void cronometro_callback(uint gpio, uint32_t events){
+  uint32_t current_time = to_us_since_boot(get_absolute_time());
+  // Verifica se passou tempo suficiente desde o último evento
+  if (current_time - last_time2 > 300000) // 300 ms de debouncing
+  {
+    last_time2=current_time;
+   if(gpio==BUTTON_B){
+     start = !start;
+     printf("Start = %d\n",start);
+     if(start){
+     //start_time = time_us_64();
+     add_repeating_timer_ms(1000, timer_cronometro, NULL, &timer1);
+     }
+    }
+    else if(gpio == JOYSTICK_BUTTON){ //JOYSTICK_BUTTON
+      puts("JOYSTICK Click");
+      selected=1;
+      start=0;
+      gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &interrupt);
+     }
+    else{ //Botão A
+      for(uint8_t i=0;i<6;i++)
+      {valor_exibido[i]=0;}
+      ssd1306_fill(&ssd, 0);
+      ssd1306_draw_string(&ssd,"000:000",10,0);
+     ssd1306_send_data(&ssd);
+    }
+  }
+}
+
 uint pwm_init_gpio(uint gpio, uint wrap) {
   gpio_set_function(gpio, GPIO_FUNC_PWM);
 
@@ -68,20 +111,88 @@ uint pwm_init_gpio(uint gpio, uint wrap) {
   return slice_num;  
 }
 
+//Provavelmente dava pra usar recursão
+void incrementar_cronometro(){
+  //if(debug_aux == 1000){start=0; return;}
+  //printf("Debug aux= %d\n",debug_aux);
+  if(valor_exibido[5]<9) valor_exibido[5]++;
+  else
+  {
+   valor_exibido[5]=0;
+   if(valor_exibido[4]<9)
+     valor_exibido[4]++;
+    else
+    {
+     valor_exibido[4]=0;
+     if(valor_exibido[3]<9)
+      valor_exibido[3]++;
+     else
+     {
+      valor_exibido[3]=0;
+      if(valor_exibido[2]<9)
+      valor_exibido[2]++;
+      else
+      {
+       valor_exibido[2]=0;
+       if(valor_exibido[1]<9)
+       valor_exibido[1]++;
+       else
+       {
+        valor_exibido[1]=0;
+        if(valor_exibido[0]<9)
+        valor_exibido[0]++;
+        else
+        {
+         puts("Valor maximo do cronometro atingido");
+         for(uint8_t i=0;i<6;i++)
+          {valor_exibido[i]=9;}
+          start=0;
+        }
+       }
+      }
+     }
+    }
+  }
+ //debug_aux++;
+  ssd1306_fill(&ssd, 0);
+  sprintf(tempo,"%d%d%d:%d%d%d",valor_exibido[0],valor_exibido[1],valor_exibido[2],valor_exibido[3],valor_exibido[4],valor_exibido[5]);
+  ssd1306_draw_string(&ssd,tempo,10,0);
+  ssd1306_send_data(&ssd);
+}
+
 void cronometro(){
+  start=0;
+  for(uint8_t i=0;i<6;i++)
+  {valor_exibido[i]=0;}
   selected=0; //Vai ser usada pra determinar quando sair da função
   ssd1306_fill(&ssd, 0);
-  //gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true, &interrupt);
+  gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true, &cronometro_callback);
   ssd1306_draw_string(&ssd,"CRONOS",30,0);
   ssd1306_send_data(&ssd);
+  gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &cronometro_callback);
+  gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &cronometro_callback);
   
   while(1){
+  //puts("Entrou no loop principal");
   if(selected == 1){
    selected=0;
    //gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, false, &interrupt);
    return;
   }
-  sleep_ms(50);
+  /*
+  if(start){
+    //printf("No loop cr\n");
+    ssd1306_fill(&ssd, 0);
+    sprintf(tempo,"%d%d%d:%d%d%d",valor_exibido[0],valor_exibido[1],valor_exibido[2],valor_exibido[3],valor_exibido[4],valor_exibido[5]);
+    ssd1306_draw_string(&ssd,tempo,10,0);
+    ssd1306_send_data(&ssd);
+    //sleep_ms(1);
+    incrementar_cronometro();
+  }
+  */
+ // current_time = to_ms_since_boot(get_absolute_time());
+//if(current_time-time_teste>=1000){start=0; puts("Passou-se 1 segundo");}
+  sleep_us(1);
  }
 }
 
@@ -217,7 +328,7 @@ int main()
       case 0: cronometro(); break;
       case 1: temporizador(); break;
     }
-   rec_pos=selected=0;
+   inicio_display=rec_pos=selected=0;
    gpio_set_irq_enabled_with_callback(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, false, &interrupt);
    draw_options();
    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &interrupt);
